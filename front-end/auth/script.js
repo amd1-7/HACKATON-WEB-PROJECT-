1678979037be6bde407ee2d3541342f3753f9aca
wahil <wahil.aissiou@epitech.eu>
2026-01-20 16:57:23 +0100

    petit changement du js

--- front-end/auth/script.js
+++ front-end/auth/script.js
@@ -3,28 +3,25 @@ const registerBtn = document.querySelector(".register-btn");
 const loginBtn = document.querySelector(".login-btn");
 
 registerBtn.addEventListener("click", () => {
-    container.classList.add("active");
+  container.classList.add("active");
 });
 
 loginBtn.addEventListener("click", () => {
-    container.classList.remove("active");
+  container.classList.remove("active");
 });
 
 //mdp login
 const loginPassword = document.getElementById("login-password");
 const toggleLogin = document.getElementById("toggle-login");
 
-
 toggleLogin.addEventListener("click", () => {
-    if (loginPassword.type === "password") {
-        loginPassword.type = "text"; // je montre le mdp
-        toggleLogin.classList.replace("fa-eye-slash", "fa-eye");
-
-    } else {
-        loginPassword.type = "password"; // je le cache
-        toggleLogin.classList.replace("fa-eye", "fa-eye-slash");
-
-    }
+  if (loginPassword.type === "password") {
+    loginPassword.type = "text"; // je montre le mdp
+    toggleLogin.classList.replace("fa-eye-slash", "fa-eye");
+  } else {
+    loginPassword.type = "password"; // je le cache
+    toggleLogin.classList.replace("fa-eye", "fa-eye-slash");
+  }
 });
 
 //mdp register
@@ -32,13 +29,13 @@ const registerPassword = document.getElementById("register-password");
 const toggleRegister = document.getElementById("toggle-register");
 
 toggleRegister.addEventListener("click", () => {
-    if (registerPassword.type === "password") {
-        registerPassword.type = "text";
-        toggleRegister.classList.replace("fa-eye-slash", "fa-eye");
-    } else {
-        registerPassword.type = "password";
-        toggleRegister.classList.replace("fa-eye", "fa-eye-slash");
-    }
+  if (registerPassword.type === "password") {
+    registerPassword.type = "text";
+    toggleRegister.classList.replace("fa-eye-slash", "fa-eye");
+  } else {
+    registerPassword.type = "password";
+    toggleRegister.classList.replace("fa-eye", "fa-eye-slash");
+  }
 });
 
 //mdp oublié
@@ -47,68 +44,68 @@ const forgotModal = document.getElementById("forgot-modal");
 const forgotClose = document.getElementById("forgot-close");
 
 forgotOpen.addEventListener("click", (e) => {
-    e.preventDefault();
-    forgotModal.style.display = "flex";
+  e.preventDefault();
+  forgotModal.style.display = "flex";
 });
 
 forgotClose.addEventListener("click", () => {
-    forgotModal.style.display = "none";
+  forgotModal.style.display = "none";
 });
 
 //LOGIN
-const loginForm = document.querySelector('.form-box.login form');
+const loginForm = document.querySelector(".form-box.login form");
 
-loginForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    const email = loginForm.querySelector('input[type="text"]').value;
-    const password = loginForm.querySelector('input[type="password"]').value;
+  const email = loginForm.querySelector('input[type="text"]').value;
+  const password = loginForm.querySelector('input[type="password"]').value;
 
-    //env vers serv
-    const rep = await fetch('http://localhost:3000/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password })
-    });
+  //env vers serv
+  const rep = await fetch("http://localhost:3000/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
 
-    const data = await rep.json();
+  const data = await rep.json();
 
-    if (rep.ok) {
-        localStorage.setItem('token', data.token);
-        window.location.href = '../todolist/index.html';
-    } else {
-        alert("Login incorrect");
-    }
+  if (rep.ok) {
+    localStorage.setItem("token", data.token);
+    window.location.href = "../todolist/index.html";
+  } else {
+    alert("Login incorrect");
+  }
 });
 
 //REGISTER
-const registerForm = document.querySelector('.form-box.register form');
+const registerForm = document.querySelector(".form-box.register form");
 
-registerForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
+registerForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    const username = registerForm.querySelectorAll('input')[0].value;
-    const email = registerForm.querySelectorAll('input')[1].value;
-    const password = registerForm.querySelectorAll('input')[2].value;
+  const username = registerForm.querySelectorAll("input")[0].value;
+  const email = registerForm.querySelectorAll("input")[1].value;
+  const password = registerForm.querySelectorAll("input")[2].value;
 
-    //env vers serv
-    const rep = await fetch('http://localhost:3000/auth/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            email,
-            name: username,
-            firstname: username,
-            password
-        })
-    });
+  //env vers serv
+  const rep = await fetch("http://localhost:3000/auth/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email,
+      name: username,
+      firstname: username,
+      password,
+    }),
+  });
 
-    const data = await rep.json();
+  const data = await rep.json();
 
-    if (rep.ok) {
-        localStorage.setItem('token', data.token);
-        window.location.href = '../todolist/index.html';
-    } else {
-        alert("Erreur inscription");
-    }
+  if (rep.ok) {
+    localStorage.setItem("token", data.token);
+    window.location.href = "../home.html";
+  } else {
+    alert("Erreur inscription");
+  }
 });
